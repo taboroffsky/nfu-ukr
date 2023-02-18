5cbbf75e6a8b968f78858a7fd717ba2be2e095ea
Andrii T <taboroffsky6@gmail.com>
2023-02-18 21:34:29 +0200

    untracked changes from 'main' restored

--- next-nfu-ukr/components/Header/header.tsx
+++ next-nfu-ukr/components/Header/header.tsx
@@ -4,16 +4,48 @@ import HeaderStyle from "./Header.module.scss";
 import Image from 'next/image';
 import flowers_corner from '../../public/icons/flowers_corner.svg'
 
+// This function's behavior needs to be applied
+const mobileMenu = () =>{
+  const hamburger = document.querySelector(".hamburger");
+  const navLinks = document.querySelector(".nav-links");
+  const links = document.querySelectorAll(".nav-links li");
+  
+  hamburger.addEventListener('click', ()=>{
+     //Animate Links
+      navLinks.classList.toggle("open");
+      links.forEach(link => {
+          link.classList.toggle("fade");
+      });
+  
+      //Hamburger Animation
+      hamburger.classList.toggle("toggle");
+  });
+}
+
 const Header = (): JSX.Element => {
  return (
     <div className={HeaderStyle.header} id="home">
-        <div className={HeaderStyle.nav_menu}id="header-nav">
-            <a href="#home"><span>Головна сторінка</span></a>
-            <a href="#gallery"><span>Галерея</span></a>
-            <a href="#about"><span>Про проект</span></a>
-            <a href="#team"><span>Наша команда</span></a>
-            <a href="#contacts"><span>Контакти</span></a>
-        </div>
+        <nav>
+      <div className="logo">
+        <img src="./images/logo/NFU_yellow.svg" alt="Logo Image"/>
+      </div>
+      <div className="hamburger">
+        <div className="line1"></div>
+        <div className="line2"></div>
+        <div className="line3"></div>
+      </div>
+      <ul className="nav-links">
+        <li><a href="#home">Головна</a></li>
+        <li><a href="#gallery">Галерея</a></li>
+        <li><a href="#about">Про проект</a></li>
+        <li><a href="#team">Наша команда</a></li>
+        <li><a href="#contacts">Контакти</a></li>
+      </ul>
+    </nav>
+    <div className="language">
+      <img src="/language/Flag_of_Ukraine.svg" alt="uk" data-google-lang="uk" className="language__img"/>
+      <img src="/language/Flag_of_the_United_Kingdom.svg" alt="en" data-google-lang="en" className="language__img"/>
+    </div>
     <div className="icons-header">
       <img className="emblem left" src="/icons/flowers_corner.svg" alt="emblem icon left"/>
       <img className="emblem right" src="/icons/flowers_corner.svg" alt="emblem icon right"/>
@@ -23,10 +55,6 @@ const Header = (): JSX.Element => {
     </div>
     <div className="description">
       <div className="description-item">Сучасне мистецтво крізь історію</div>
-    </div>
-    <div className="language">
-      <img src="/language/Flag_of_Ukraine.svg" alt="uk" data-google-lang="uk" className="language__img"/>
-      <img src="/language/Flag_of_the_United_Kingdom.svg" alt="en" data-google-lang="en" className="language__img"/>
     </div>
   </div>
  )
