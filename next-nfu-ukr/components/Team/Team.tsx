933b8297ff105a86a1859b991f545f63a71f449b
Taras Ishchuk <58121868+ishchuktaras@users.noreply.github.com>
2023-03-30 19:54:37 +0000

    last

--- next-nfu-ukr/components/Team/Team.tsx
+++ next-nfu-ukr/components/Team/Team.tsx
@@ -5,49 +5,50 @@ const teamMembers = [
   {
     name: "Андрій Костін",
     role: "Засновник проекту",
-    image: "images/avatars/kostin.jpg",
+    image: "avatars/kostin.jpg"
   },
   {
     name: "Олександр Лук'янюк",
     role: "2D художник",
-    image: "images/avatars/lukianiuk.jpg",
+    image: "avatars/lukianiuk.jpg"
   },
   {
     name: "Тарас Іщук",
     role: "Веб розробник",
-    image: "images/avatars/ishchuk.jpg",
+    image: "avatars/ishchuk.jpg"
   },
   {
     name: "Влад Орсагош",
     role: "Художник",
-    image: "images/avatars/orsahosh.jpg",
+    image: "avatars/orsahosh.jpg"
   },
   {
     name: "Марія Андреченко",
     role: "Маркетолог",
-    image: "images/avatars/andrechenko.jpg",
-  },
+    image: "avatars/andrechenko.jpg"
+  }
 ];
 
-const Team = (teamMembers) => {
+const Team = () => {
   return (
-    
-    <div className={`${TeamStyle.team} container`} id="team">
+    <section className={${TeamStyle.team} container} id="team">
       <h1>Наша команда</h1>
-      <p>
-        Наша мрія, як і мрія багатьох, — мир в Україні. Мир усьому світу! <br />{" "}
-        Щиро дякуємо всім за підтримку та допомогу Україні!
-      </p>
-
-      <div className={TeamStyle.team_card}>
-        {teamMembers.map((teamMember) =>{
-          return < teamMember = {teamMember} />
-        })}
-
-      </div>
-    </div>
-    
-  );
-};
+      <p>Наша мрія, як і мрія багатьох, — мир в Україні. Мир усьому світу! <br /> Щиро дякуємо всім за
+        підтримку та допомогу Україні!</p>
+      {
+        teamMembers.map((teamMember) =>
+        (
+          <div className={TeamStyle.team_card}>
+            <img src={teamMember.image} alt="Avatar" />
+            <div>
+              <h4>{teamMember.name}</h4>
+              <p>{teamMember.role}</p>
+            </div>
+          </div>
+        ))
+      }
+    </section>
+  )
+}
 
 export default Team;
