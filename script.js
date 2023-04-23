af72d08e4489e274873aa38c74d73d3c99bccaf1
Taras Ishchuk <58121868+ishchuktaras@users.noreply.github.com>
2023-04-23 10:52:10 +0000

    Popup Card style & text bug fixing

--- script.js
+++ script.js
@@ -18,107 +18,107 @@ hamburger.addEventListener('click', ()=>{
 
 // Main site translator
 
-const googleTranslateConfig = {
-	/* Original language */
-	lang: "uk",
+// const googleTranslateConfig = {
+// 	/* Original language */
+// 	lang: "uk",
 
-	/* The language we translate into on the first visit*/
-	langFirstVisit: 'en',
-};
+// 	/* The language we translate into on the first visit*/
+// 	langFirstVisit: 'en',
+// };
 
-document.addEventListener("DOMContentLoaded", (event) => {
+// document.addEventListener("DOMContentLoaded", (event) => {
 
-	/* Connecting the google translate widget */
-	let script = document.createElement("script");
-	script.src = `//translate.google.com/translate_a/element.js?cb=TranslateWidgetIsLoaded`;
-	document.getElementsByTagName("head")[0].appendChild(script);
-});
+// 	/* Connecting the google translate widget */
+// 	let script = document.createElement("script");
+// 	script.src = `//translate.google.com/translate_a/element.js?cb=TranslateWidgetIsLoaded`;
+// 	document.getElementsByTagName("head")[0].appendChild(script);
+// });
 
-function TranslateWidgetIsLoaded() {
-	TranslateInit(googleTranslateConfig);
-}
+// function TranslateWidgetIsLoaded() {
+// 	TranslateInit(googleTranslateConfig);
+// }
 
-function TranslateInit(config) {
-	if (config.langFirstVisit && !Cookies.get("googtrans")) {
+// function TranslateInit(config) {
+// 	if (config.langFirstVisit && !Cookies.get("googtrans")) {
 
-		/* If the translation language is installed for the first visit and cookies are not assigned */
-		TranslateCookieHandler("/auto/" + config.langFirstVisit);
-	}
+// 		/* If the translation language is installed for the first visit and cookies are not assigned */
+// 		TranslateCookieHandler("/auto/" + config.langFirstVisit);
+// 	}
 
-	let code = TranslateGetCode(config);
+// 	let code = TranslateGetCode(config);
 
-	TranslateHtmlHandler(code);
+// 	TranslateHtmlHandler(code);
 
-	if (code == config.lang) {
+// 	if (code == config.lang) {
 
-		/* If the default language is the same as the language we are translating into, then we clear the cookies */
-		TranslateCookieHandler(null, config.domain);
-	}
+// 		/* If the default language is the same as the language we are translating into, then we clear the cookies */
+// 		TranslateCookieHandler(null, config.domain);
+// 	}
 
 
-	/* Initialize the widget with the default language */
-	new google.translate.TranslateElement({
-		pageLanguage: config.lang,
-	});
+// 	/* Initialize the widget with the default language */
+// 	new google.translate.TranslateElement({
+// 		pageLanguage: config.lang,
+// 	});
 
-	/* Assigning a handler to the flags */
-	TranslateEventHandler("click", "[data-google-lang]", function (e) {
-		TranslateCookieHandler(
-			"/" + config.lang + "/" + e.getAttribute("data-google-lang"),
-			config.domain
-		);
+// 	/* Assigning a handler to the flags */
+// 	TranslateEventHandler("click", "[data-google-lang]", function (e) {
+// 		TranslateCookieHandler(
+// 			"/" + config.lang + "/" + e.getAttribute("data-google-lang"),
+// 			config.domain
+// 		);
 
-		/* Reloading the page */
-		window.location.reload();
-	});
-}
+// 		/* Reloading the page */
+// 		window.location.reload();
+// 	});
+// }
 
-function TranslateGetCode(config) {
+// function TranslateGetCode(config) {
 
-	/* If there are no cookies, then we pass the default language */
-	let lang =
-		Cookies.get("googtrans") != undefined && Cookies.get("googtrans") != "null"
-			? Cookies.get("googtrans")
-			: config.lang;
-	return lang.match(/(?!^\/)[^\/]*$/gm)[0];
-}
+// 	/* If there are no cookies, then we pass the default language */
+// 	let lang =
+// 		Cookies.get("googtrans") != undefined && Cookies.get("googtrans") != "null"
+// 			? Cookies.get("googtrans")
+// 			: config.lang;
+// 	return lang.match(/(?!^\/)[^\/]*$/gm)[0];
+// }
 
-function TranslateCookieHandler(val, domain) {
+// function TranslateCookieHandler(val, domain) {
 
-	/* Writing down cookies /language_for_translation/the_language_we_are_translating_into */
-	Cookies.set("googtrans", val);
-	Cookies.set("googtrans", val, {
-		domain: "." + document.domain,
-	});
+// 	/* Writing down cookies /language_for_translation/the_language_we_are_translating_into */
+// 	Cookies.set("googtrans", val);
+// 	Cookies.set("googtrans", val, {
+// 		domain: "." + document.domain,
+// 	});
 
-	if (domain == "undefined") return;
+// 	if (domain == "undefined") return;
 
-	/* Writing down cookies for the domain, if it is assigned in the config */
-	Cookies.set("googtrans", val, {
-		domain: domain,
-	});
+// 	/* Writing down cookies for the domain, if it is assigned in the config */
+// 	Cookies.set("googtrans", val, {
+// 		domain: domain,
+// 	});
 
-	Cookies.set("googtrans", val, {
-		domain: "." + domain,
-	});
-}
+// 	Cookies.set("googtrans", val, {
+// 		domain: "." + domain,
+// 	});
+// }
 
-function TranslateEventHandler(event, selector, handler) {
-	document.addEventListener(event, function (e) {
-		let el = e.target.closest(selector);
-		if (el) handler(el);
-	});
-}
+// function TranslateEventHandler(event, selector, handler) {
+// 	document.addEventListener(event, function (e) {
+// 		let el = e.target.closest(selector);
+// 		if (el) handler(el);
+// 	});
+// }
 
-function TranslateHtmlHandler(code) {
+// function TranslateHtmlHandler(code) {
 
-	/* We get the language to which we translate and produce the necessary manipulations with DOM */
-	if (document.querySelector('[data-google-lang="' + code + '"]') !== null) {
-		document
-			.querySelector('[data-google-lang="' + code + '"]')
-			.classList.add("language__img_active");
-	}
-}
+// 	/* We get the language to which we translate and produce the necessary manipulations with DOM */
+// 	if (document.querySelector('[data-google-lang="' + code + '"]') !== null) {
+// 		document
+// 			.querySelector('[data-google-lang="' + code + '"]')
+// 			.classList.add("language__img_active");
+// 	}
+// }
 
 
 // Gallery Card
