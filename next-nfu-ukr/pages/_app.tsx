1c0240d855232679670aff4a4e27b1f82621a4b3
taboroffsky <taboroffsky6@gmail.com>
2023-12-01 23:20:47 +0200

    minor fix

--- next-nfu-ukr/pages/_app.tsx
+++ next-nfu-ukr/pages/_app.tsx
@@ -5,12 +5,12 @@ import { NotificationProvider } from 'web3uikit'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <NotificationProvider>
-      <MoralisProvider initializeOnMount={false}>
-        <>
+    <>
+      <NotificationProvider>
+        <MoralisProvider initializeOnMount={false}>
           <Component {...pageProps} />
-        </>
-      </MoralisProvider>
-    </NotificationProvider>
+        </MoralisProvider>
+      </NotificationProvider>
+    </>
   )
 }
